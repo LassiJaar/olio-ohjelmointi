ed6aeacfffc7a64dfee6d562b6efc49ee76b37cf
LassiJaara <Lassi.jaara2004@gmail.com>
2025-11-26 15:58:29 +0200

    Tehtävän h6a palautus

---
+++ h6a/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "myclass.h"
+
+using namespace std;
+
+int main()
+{
+    MyClass esine;
+    esine.raiseMySignal();
+    //cout << "Hello World!" << endl;
+
+
+    return 0;
+}
